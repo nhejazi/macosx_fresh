4ab1b4f75e6e53da592ada46758aa264135f4400
Nima Hejazi <nima.hejazi7@gmail.com>
2016-02-15 21:30:47 -0800

    Create 5_getHammerspoon.sh

---
+++ 5_getHammerspoon.sh
@@ -0,0 +1,10 @@
+# install Lua language, Hammerspoon utility
+brew install lua
+brew cask install hammerspoon
+
+# set up preferred configuration of Hammerspoon
+if [-e ~/.hammerspoon]; then 
+  do rm -rf ~/.hammerspoon; 
+fi
+
+git clone git@github.com:nhejazi/myhammerspoon.git ~/.hammerspoon
