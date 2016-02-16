c5bba7045965c4548819c4ae633ca9fbd72cbb2e
Nima Hejazi <nima.hejazi7@gmail.com>
2016-02-15 21:21:50 -0800

    Update and rename 1_initial.sh to 1_genBrew.sh

---
+++ 1_genBrew.sh
@@ -0,0 +1,15 @@
+# get Homebrew utility
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# get Homebrew essentials
+brew update && brew upgrade
+brew doctor
+brew tap caskroom/cask
+brew tap homebrew/science
+brew tap caskroom/fonts
+
+# get XCode and core Homebrew tools
+xcode-select --install
+brew install wget git coreutils zmq
+brew install libsvg curl libxml2 gdal geos boost
+brew cask install font-fira-code font-fontawesome
