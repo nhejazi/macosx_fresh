e0e54ca2070a8c745d9ac9e7d0b54adb50911a62
Nima Hejazi <nima.hejazi7@gmail.com>
2016-02-15 21:43:49 -0800

    Create 8_setupPython.sh

---
+++ 8_setupPython.sh
@@ -0,0 +1,6 @@
+# set up a few Python-specific things with Brew
+brew install matplotlib --with-cairo --with-tex
+brew install libtiff libjpeg webp little-cms2
+brew install imagemagick --with-fftw --with-librsvg --with-x11
+brew install graphviz --with-librsvg --with-x11
+brew install cairo py2cairo qt pyqt
