386fb9a525160f7c12aec52588ba794cacb7d34d
Nima Hejazi <nima.hejazi7@gmail.com>
2016-02-27 09:53:03 -0800

    formality

--- 2_getRuby.sh
+++ 2_getRuby.sh
@@ -1,4 +1,6 @@
-# install a "user" Ruby (to replace "system" Ruby)
+#!/usr/bin/bash
+
+# install a "user" Ruby (replace "system" Ruby)
 brew install rbenv ruby-build
 rbenv install 2.2.3
 rbenv global 2.2.3
