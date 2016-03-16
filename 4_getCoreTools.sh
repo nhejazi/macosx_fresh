a440e3709c233619f4beef1a624d4aa6adf6d71f
Nima Hejazi <nima.hejazi7@gmail.com>
2016-03-16 16:13:43 -0700

    move stuff from python script

--- 4_getCoreTools.sh
+++ 4_getCoreTools.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/bash
 
-# install iTerm, Atom text editor, and xquartz + java
+# install iTerm, Atom (text editors)
 brew cask install iterm2
-
 brew cask install atom
 
+# install Java and Xquartz
 brew install Caskroom/cask/xquartz
 brew cask install java
 
-brew install pandoc
-brew install node
+# more scientific computing tools
+brew install pandoc cairo qt node
+brew install libtiff libjpeg webp little-cms2
+brew install graphviz --with-librsvg --with-x11
+brew install imagemagick --with-fftw --with-librsvg --with-x11
