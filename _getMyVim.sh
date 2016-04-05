0c12b1a0f28dabcfbf0c1484b6931859c8df9020
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-05 11:25:57 -0700

    remove neovim linkapps

--- _getMyVim.sh
+++ _getMyVim.sh
@@ -5,7 +5,7 @@ export PATH=/usr/local/bin:$PATH
 brew update
 brew install vim && brew install macvim
 brew install neovim/neovim/neovim
-brew linkapps macvim neovim
+brew linkapps macvim
 
 # setup preferred configuration for Vim and MacVim
 if [ -e ~/.vim ]; then 
