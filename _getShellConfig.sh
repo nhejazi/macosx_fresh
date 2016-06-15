0fcffd9bc3c765ca2eccfd6792a6bf5d5b2c73c0
Nima Hejazi <nima.hejazi7@gmail.com>
2016-06-14 18:34:47 -0700

    Delete _getShellConfig.sh

--- _getShellConfig.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-# set up configuration scripts for Bash shell
-cd
-mkdir git-repos
-git clone git@github.com:nhejazi/mydotfiles.git ~/git-repos/mydotfiles
-cd ~/git-repos/mydotfiles
-sh _setup.sh
