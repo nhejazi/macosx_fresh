c3cbae1c01a42f60579f6f4bd512c5a30687232c
Nima Hejazi <nima.hejazi7@gmail.com>
2016-06-14 18:27:56 -0700

    add termpdf tool

--- _getCoreTools.sh
+++ _getCoreTools.sh
@@ -1,22 +1,25 @@
 #!/usr/local/bin/bash
 
+# install Java and Xquartz
+brew cask install xquartz
+brew cask install java
+
 # install terminal and text editor
 brew cask install iterm2
 brew cask install atom
 brew cask install black-screen
 
-# install Java and Xquartz
-brew cask install xquartz
-brew cask install java
-
-# more scientific computing tools
+# add more scientific computing tools
 brew install pandoc cairo qt node
 brew install libtiff libjpeg webp little-cms2
 brew install graphviz --with-librsvg --with-x11
 brew install imagemagick --with-fftw --with-librsvg --with-x11
 
-# adding dependencies for using termpdf tool
-brew install bash gs imagemagick poppler pdfgrep djvulibre selecta
+# add "termpdf" tool and dependencies
+brew install poppler ghostscript djvulibre
+git clone https://github.com/dsanson/termpdf.git ~/.termpdf
+ln -s ~/.termpdf/termpdf /usr/local/bin/termpdf
+chmod u+x /usr/local/bin/termpdf
 
 # installing tool for StackOverflow support from terminal
 npm install -g how2
