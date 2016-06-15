3b1d2841083cb83184cc88daeefd8dd08b284869
Nima Hejazi <nima.hejazi7@gmail.com>
2016-06-14 18:32:27 -0700

    add "User" Ruby

--- _getCoreLangs.sh
+++ _getCoreLangs.sh
@@ -1,5 +1,10 @@
 #!/usr/local/bin/bash
 
+# install "user Ruby" to replace "system Ruby"
+brew install rbenv ruby-build
+rbenv install 2.2.3
+rbenv global 2.2.3
+
 # install Python 2 and 3
 brew install python
 brew install python3
