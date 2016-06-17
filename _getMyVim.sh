e3f20ec7becb4265a39b821b70ade40cf343a2ac
Nima Hejazi <nima.hejazi7@gmail.com>
2016-06-17 12:30:38 -0700

    Update _getMyVim.sh

--- _getMyVim.sh
+++ _getMyVim.sh
@@ -16,9 +16,14 @@ if [ -e ~/.vimrc]; then
   rm -f ~/.vimrc; 
 fi
 
+# get and setup config files for using Vim
 git clone http://github.com/nhejazi/myvimconfig.git ~/.vim
 sh ~/.vim/_setup.sh
 cd ~/.vim; sh _update.sh; cd
 
+# get and setup config files for using Neovim
 git clone http://github.com/nhejazi/myneovimconfig.git ~/.config/nvim
 sh ~/.config/nvim/_setup.sh
+
+# get the diff-so-fancy tool for `git diff`
+npm install -g diff-so-fancy
