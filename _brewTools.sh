1f8a6763b5621e6e60f8b76d83be9a4508ae1792
Nima Hejazi <nima.hejazi7@gmail.com>
2016-11-28 16:56:38 -0800

    add anybar, rm sourcetree + blackscreen

--- _brewTools.sh
+++ _brewTools.sh
@@ -1,27 +1,28 @@
 #!/usr/local/bin/bash
 
 
-# add Google Chrome, Dropbox, Hammerspoon
-brew cask install google-chrome
-brew cask install hammerspoon
-brew cask install dropbox
-
-
 # add Java and X-quartz
 brew cask install xquartz
 brew cask install java
 
 
+# add Google Chrome, Dropbox, Hammerspoon, AnyBar
+brew cask install google-chrome
+brew cask install hammerspoon
+brew cask install dropbox
+brew cask install anybar
+
+
 # add terminal emulators
 brew cask install hyper
 brew cask install iterm2
-brew cask install black-screen
+#brew cask install black-screen
 
 
 # add Atom, GitHub Desktop, SourceTree
 brew cask install atom
 brew cask install github-desktop  # GUI for Git
-brew cask install sourcetree  # GUI for Git
+#brew cask install sourcetree  # GUI for Git
 
 
 # add screen/brightness adjustment tools
@@ -37,7 +38,7 @@ chmod u+x /usr/local/bin/termpdf
 
 
 # add command line tools for Genomics
+brew install kallisto  # for pseudo-alignment
 brew install samtools  # for alignments
 brew install bedtools  # for annotations
 brew install cufflinks  # for alignments
-brew install kallisto # for pseudoalignment
