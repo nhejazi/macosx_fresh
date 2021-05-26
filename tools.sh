826ff558a5d20aa71f6f328b6a954df52637f758
Nima Hejazi <nh@nimahejazi.org>
2021-05-26 15:35:02 -0700

    add joplin

--- tools.sh
+++ tools.sh
@@ -7,14 +7,17 @@ brew install --cask firefox
 #brew install --cask hammerspoon
 brew install --cask dropbox
 brew install --cask spotify
-brew install --cask skype
-brew install --cask slack
+#brew install --cask skype
+#brew install --cask slack
 brew install --cask iterm2       # macOS-only terminal emulator
 brew install --cask terminus     # cross-platform "modern" terminal emulator
 brew install --cask lumen        # brightness/lighting adjustment
 
 # add jrnl for journaling/productivity
 brew install jrnl
+
+# add joplin for notetaking
+brew install joplin
 
 # resource monitors showing usage and stats for processor, memory, disks, etc
 brew install glances
