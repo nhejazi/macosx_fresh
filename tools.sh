3ca96c3207a44f5083cf4ce826fe7fd9bf798e54
nima hejazi <nh@nimahejazi.org>
2021-09-14 16:11:19 -0400

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -4,20 +4,16 @@
 brew install --cask xquartz
 brew install --cask firefox
 #brew install --cask google-chrome
-#brew install --cask hammerspoon
 brew install --cask dropbox
 brew install --cask spotify
 #brew install --cask skype
 #brew install --cask slack
 brew install --cask iterm2       # macOS-only terminal emulator
-brew install --cask terminus     # cross-platform "modern" terminal emulator
+brew install --cask tabby        # cross-platform "modern" terminal emulator
 brew install --cask lumen        # brightness/lighting adjustment
 
 # add jrnl for journaling/productivity
 brew install jrnl
-
-# add joplin for notetaking
-brew install joplin
 
 # resource monitors showing usage and stats for processor, memory, disks, etc
 brew install glances
