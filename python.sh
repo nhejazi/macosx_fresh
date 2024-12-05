5fc82fe818b992cc4f5daed065ecb522dec8d93e
nima hejazi <nh@nimahejazi.org>
2024-12-05 18:07:49 -0500

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,7 +1,7 @@
 # install Python and set up base environment via pyenv
 pyenv install 3.11.11
 pyenv virtualenv 3.11.11 base_env
-pyenv global base
+pyenv global base_env
 
 # upgrade pip first
 python -m pip install --upgrade pip
