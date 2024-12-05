680464c12121aafd3a5aeb4c65b7f181c7efec54
Nima Hejazi <nh@nimahejazi.org>
2024-12-05 18:04:30 -0500

    update and simplify python

--- python.sh
+++ python.sh
@@ -1,17 +1,10 @@
 # install Python and set up base environment via pyenv
-pyenv install 3.10.5
-pyenv virtualenv 3.10.5 base_env
+pyenv install 3.11.11
+pyenv virtualenv 3.11.11 base_env
 pyenv global base
 
 # upgrade pip first
 python -m pip install --upgrade pip
-
-# nicer displays of system information
-#python -m pip install glances
-#python -m pip install bpytop --upgrade
-
-# manpages and how-tos
-python -m pip install tldr howdoi
 
 # REPLs for Python and R
 python -m pip install ipython radian
