72143cc832b82a4b9d1609fdb5c6ffa347133d2f
Nima Hejazi <nh@nimahejazi.org>
2024-12-05 17:41:28 -0500

    drop zed and rstudio

--- tools.sh
+++ tools.sh
@@ -11,10 +11,8 @@ brew install --cask slack
 # add development tools
 brew install --cask iterm2
 brew install --cask alacritty
-brew install --cask zed
 brew install --cask visual-studio-code
-#brew install --cask rstudio
-#brew install --cask positron
+brew install --cask positron
 brew install --cask quarto
 
 # resource monitors
